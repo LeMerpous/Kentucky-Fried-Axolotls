890f000a5435e3b67aeed607e43c08cd3888a42d
NotMerpBtw <86994527+Merpous@users.noreply.github.com>
2022-07-14 22:35:37 -0400

    Update AxolotlFoodComponents.java

--- src/main/java/com/github/merpous/axolotl/AxolotlFoodComponents.java
+++ src/main/java/com/github/merpous/axolotl/AxolotlFoodComponents.java
@@ -2,11 +2,9 @@ package com.github.merpous.axolotl;
 
 
 import net.minecraft.item.FoodComponent;
-import net.minecraft.item.FoodComponents;
-import net.minecraft.item.ItemStack;
-import net.minecraft.entity.effect.StatusEffectInstance;
 
 public class AxolotlFoodComponents {
 
-    public static final FoodComponent FRIED_AXOLOTL = (new FoodComponent.Builder()).snack().hunger(10).saturationModifier(3).build();
+    public static final FoodComponent FRIED_AXOLOTL = (new FoodComponent.Builder()).hunger(8).saturationModifier(0.8f).build();
+
 }
